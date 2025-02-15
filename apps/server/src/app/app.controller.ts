14b6b199c40ead89ace1393fbaf3fc7070e2ae7c
Ryan Moon <ryanmoondevelopment@gmail.com>
2025-02-14 22:10:28 -0600

    setting up more backend

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -5,8 +5,14 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('/healthCheck')
+  healthCheck() {
+    this.appService.healthCheck();
+  }
+
   @Get()
   getData() {
     return this.appService.getData();
   }
 }
+
--- apps/server/src/app/app.service.ts
+++ apps/server/src/app/app.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { EponaClient } from '@epona/epona-client';
+import { SuccessResponse } from './Responses';
 
 @Injectable()
 export class AppService {
+
+  healthCheck(){
+    return new SuccessResponse({success:true})
+  }
+
   async getData() {
     const epona = new EponaClient();
-    const a = await epona.converse({ message: "what is 3 plus 5" });
-    console.log(a)
+    // const a = await epona.converse({ message: "what is 3 plus 5" });
+    // console.log(a)
     return { message: 'Hello API' };
   }
 }
