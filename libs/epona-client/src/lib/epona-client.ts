09d2b4a58288a663e8b3cc77c51ca084c8446096
Ryan Moon <ryanmoondevelopment@gmail.com>
2025-02-15 22:53:23 -0600

    some progress(?) on typescript

--- libs/epona-client/src/lib/epona-client.ts
+++ libs/epona-client/src/lib/epona-client.ts
@@ -1,60 +1,118 @@
 import { OllamaClient, OllammaModels } from '@epona/ollama';
-import { EponaChatParams } from './types';
-import eponaTools, { ToolOrganizer } from './eponaTools';
-import { ChatResponse, Message, ToolCall } from 'ollama';
+import { Message } from 'ollama';
+import * as fs from 'node:fs';
+import { AnyTool } from 'bee-agent-framework/tools/base';
+import { UnconstrainedMemory } from 'bee-agent-framework/memory/unconstrainedMemory';
+import { BeeAgent } from 'bee-agent-framework/agents/bee/agent';
 
 export default class EponaClient {
-  private readonly ollamaClient = OllamaClient;
-  private readonly baseModel = OllammaModels.llama_3p2;
-  private readonly tools: ToolOrganizer = eponaTools;
+  private readonly model = OllammaModels.llama_3p2;
+  private readonly llm = OllamaClient(this.model);
+  private readonly tools: AnyTool[] = [];
+  private memory = new UnconstrainedMemory();
+  private agent: BeeAgent;
 
-  private async chat(messages: Message[]) {
-    return this.ollamaClient.chat({
-      model: this.baseModel,
-      tools: this.tools.definitions,
-      messages,
+  private memoryFileName = 'eponaMemory.json';
+
+  constructor() {
+    this.agent = new BeeAgent({
+      llm: this.llm,
+      memory: this.memory,
+      tools: this.tools,
     });
   }
 
-  async runTools({response,toolCalls}:{response:ChatResponse,toolCalls:ToolCall[]}) {
-    const messages: Message[] = [];
-
-    // Process tool calls from the response
-    for (const tool of toolCalls) {
-      const functionToCall = this.tools.getFunctionByFunctionName(tool.function.name);
-      if (functionToCall) {
-        console.log('Epona is calling function:', tool.function.name,' Arguments:', tool.function.arguments);
-        console.log();
-        const output:string = functionToCall(tool.function.arguments);
-        console.log('Function output:', output);
-
-        // Add the function response to messages for the model to use
-        messages.push(response.message);
-        messages.push({
-          role: 'tool',
-          content: output,
-        });
-      } else {
-        console.log('Function', tool.function.name, 'not found');
-      }
+  public saveMemory() {
+    try {
+      const serializedMemory = this.memory.createSnapshot();
+      fs.writeFileSync(this.memoryFileName, JSON.stringify(serializedMemory));
+      return {
+        success: true,
+        message: "Epona's Memory has been saved successfully.",
+      };
+    } catch (error) {
+      return {
+        success: true,
+        message: "An ERROR occured saving Epona's Memory",
+      };
     }
-    return messages;
   }
 
-  async converse(input: EponaChatParams) {
-    const response = await this.chat([
-        {
-          role: 'user',
-          content: input?.message,
-          images: input?.images,
-        },
-      ],
-    );
-
-    if (response.message.tool_calls) {
-      const toolMessages =  await this.runTools({ response, toolCalls: response.message.tool_calls });
-      return this.chat(toolMessages);
+  private loadMemory() {
+    try {
+      const eponasSavedMemory = fs.readFileSync(this.memoryFileName, 'utf-8');
+      const serializedMemory = JSON.parse(eponasSavedMemory);
+      this.memory.loadSnapshot(serializedMemory);
+      return {
+        success: true,
+        message: "Epona's Memory has been loaded successfully.",
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        success: false,
+        message: "An ERROR occurred loading Epona's Memory",
+      };
     }
-    return response;
   }
+
+  private async chat(messages: Message[]) {
+    // return this.ollamaClient.chat({
+    //   model: this.baseModel,
+    //   tools: this.tools.definitions,
+    //   messages,
+    // });
+  }
+
+  // private async streamChat(messages: Message[]) {
+  //   const a = await this.ollamaClient.chat({
+  //     model: this.baseModel,
+  //     tools: this.tools.definitions,
+  //     messages,
+  //     stream:true
+  //   });
+  // }
+
+  // async runTools({response,toolCalls}:{response:ChatResponse,toolCalls:ToolCall[]}) {
+  //   const messages: Message[] = [];
+  //
+  //   // Process tool calls from the response
+  //   for (const tool of toolCalls) {
+  //     const functionToCall = this.tools.getFunctionByFunctionName(tool.function.name);
+  //     if (functionToCall) {
+  //       console.log('Epona is calling function:', tool.function.name,' Arguments:', tool.function.arguments);
+  //       console.log();
+  //       const output:string = functionToCall(tool.function.arguments);
+  //       console.log('Function output:', output);
+  //
+  //       // Add the function response to messages for the model to use
+  //       messages.push(response.message);
+  //       messages.push({
+  //         role: 'tool',
+  //         content: output,
+  //       });
+  //     } else {
+  //       console.log('Function', tool.function.name, 'not found');
+  //     }
+  //   }
+  //   return messages;
+  // }
+
+  // async converse(input: EponaChatParams) {
+  //   const response = await this.chat([
+  //       {
+  //         role: 'user',
+  //         content: input?.message,
+  //         images: input?.images,
+  //       },
+  //     ],
+  //   );
+  //
+  //   return response;
+  //   // if (response.message.tool_calls) {
+  //   //   const toolMessages =  await this.runTools({ response, toolCalls: response.message.tool_calls });
+  //   //   return this.chat(toolMessages);
+  //   // }
+  //   // return response;
+  // }
 }
--- libs/ollama/src/lib/ollamaClient.ts
+++ libs/ollama/src/lib/ollamaClient.ts
@@ -1,5 +1,16 @@
 import { Ollama } from 'ollama';
+import { OllamaChatModel } from "bee-agent-framework/adapters/ollama/backend/chat";
+import { OllammaModels } from './models';
 
+const OLLAMA_SERVER = 'http://10.1.2.38:11434';
 
-export default new Ollama({ host: 'http://127.0.0.1:11434' })
+export default (model:OllammaModels)=> {
+  return new OllamaChatModel({
+    modelId: model,
+    client: new Ollama({
+      host: OLLAMA_SERVER,
+      // fetch: noTimeoutFetch
+    }),
+  });
+}
 
